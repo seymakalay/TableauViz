edb4fb3433546f39653fd722e9f2d210b922169e
seymakalay <seymakalay@hotmail.com>
2023-01-20 12:50:24 +0100

    commit

--- nyc13.R
+++ nyc13.R
@@ -1,6 +1,7 @@
-library("readxl"); library(dplyr)
+library("readxl"); library(dplyr); library(tidyr); library(ggplot2)
 
 flights <- read_excel("nycflights13.lon.lat.xlsx", sheet = "flights")
+weather <- read_excel("nycflights13.lon.lat.xlsx", sheet = "weather")
 
 #  8255 of departures getting cancelled
 sum(is.na(flights$dep_time)) 
@@ -9,3 +10,12 @@ sum(is.na(flights$dep_time))
 byMon_EWR_total <- group_by(flights[flights$origin == "EWR",],month)
 ( sumMon_EWR_total <- summarize(byMon_EWR_total,count=n()) )    
 sum (sumMon_EWR_total)
+
+
+flights %>%
+  left_join(weather, by = c("origin", "time_hour")) %>%
+  select(dep_delay, visib) %>%
+  drop_na() -> d
+d
+
+ggplot(d, aes(x = visib, y = dep_delay)) + geom_point() + geom_smooth(se = FALSE)
